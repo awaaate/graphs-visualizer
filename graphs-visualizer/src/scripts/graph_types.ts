001a42583908488fb46d599345d8d12dca07b326
Alua1714 <eloipages@gmail.com>
2021-11-16 20:30:10 +0100

    commit HE HECHO COMMIT XDDD

--- graphs-visualizer/src/scripts/graph_types.ts
+++ graphs-visualizer/src/scripts/graph_types.ts
@@ -12,59 +12,98 @@ interface Coords {
 
 export class Node {
     element?: HTMLElement;
-    constructor(
-        public id: number,
-        public adjList: Edge[],
-        public coords: Coords
-    ) {
+    public id: number;
+    public adjList: Edge[];
+
+    public coords: Coords;
+
+    public visited: boolean;
+    public distance: number;
+    constructor(id:number) {
         this.element = document.createElement("span");
         this.element.id = id.toString();
         this.element.textContent = id.toString();
+
+        this.adjList = [];
+        this.id = id;
+
+        this.visited = false;
+        this.distance = INF;
     }
 }
-
+//EVERYTHING WORKS WITH 0 INDEXATION;
 export class Graph{
     size:number;
     clean:boolean;
-    visited:boolean[];
-    distance:number[];
 
     id_node:Node[]; //means gives and id, returns node;
-    constructor(){
+    constructor(n:number){
         this.size = 0;
         this.clean = false;
-    }
-    create(n){
         this.size = n;
+        this.create(n);
+    }
+    create(n:number){
+        this.size = n;
+        for(let i=0; i<n;++i){
+            this.id_node.push(new Node(i));
+        }
     }
     reset(){
-        this.visited = Array.from({length: this.size});
-        this.distance = Array.from({length: this.size});
-        this.distance.map((item,index)=> {return INF});
+        for(let i = 0; i<this.size; ++i){
+            this.id_node[i].visited = false;
+            this.id_node[i].distance = INF;
+        }
         this.clean = true;
+    }
+    addNode(a:number,b:number,w=1, undirected:boolean = true){
+        this.id_node[a].adjList.push({weight: w, to:b});
+        if(undirected){
+            this.id_node[b].adjList.push({weight:w, to:a});
+        }
+        this.size++;
     }
     runBFS(sourceId:number, targetId:number):void{
         if(!this.clean){
             this.reset();
         }
-        const par:number[] = Array.from({length: this.size});
+        this.clean = false;
         const q = new Queue();
+        const q2 = new Queue();
+
+        q2.push(1);
         q.push(sourceId);
-        this.distance[sourceId] = 1;
+        this.id_node[sourceId].distance = 1;
+
+        let currentStage:number = 1;
+        let found:boolean = false;
+
         while(!q.empty()){
             let curr = q.front();
-            q.pop();
-            for(const edge of this.id_node[curr].adjList){
-                if(this.distance[edge.to] != INF){
-                    this.distance[edge.to] = this.distance[curr] + 1;
-                    par[edge.to] = curr;
-                    q.push(edge.to);
-                    if(edge.to == targetId){
-                        q.delete();
-                        break;
-                    }
+            if(currentStage != q2.front()){
+                currentStage = q2.front();
+                if(found){
+                    break;
                 }
             }
+            this.id_node[curr].visited = true;
+            q.pop();
+            for(let to of this.id_node[curr].adjList){
+                if(!this.id_node[curr].visited){
+                    this.id_node[to.to].distance = this.id_node[curr].distance;
+                    if(to.to == targetId){
+                        found = true;
+                    }
+                    q.push(to.to);
+                    q2.push(q2.front() + 1);
+                }
+            }
+            q2.pop();
         }
+
+        console.log(this.id_node[targetId].distance);
     }
 }
+
+
+
